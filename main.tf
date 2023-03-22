fb14731dcf4bc93691e3cf0a86f2add5d8e24817
Pierre Lebigre <pierre.lebigre@outlook.fr>
2023-03-22 15:59:37 +0100

    Init branch

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.48.0"
+    }
+  }
+
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_service_plan" "app-plan" {
+  name                = "plan-${var.project_name}${var.environment_suffix}"
+  resource_group_name = data.azurerm_resource_group.rg-maalsi.name
+  location            = data.azurerm_resource_group.rg-maalsi.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "web-${var.project_name}${var.environment_suffix}"
+  resource_group_name = data.azurerm_resource_group.rg-maalsi.name
+  location            = data.azurerm_resource_group.rg-maalsi.location
+  service_plan_id     = azurerm_service_plan.app-plan.id
+  site_config {}
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "environment_suffix" {
+  type = string
+  description = "procure le suffixe indiquant l'environnement cible"
+}
+variable "location" {
+  type = string
+  default = "West Europe"
+}
+variable "project_name" {
+  type = string
+}
