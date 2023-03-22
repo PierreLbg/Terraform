154bc97fd32ba5027993bf9137b3385057eaab25
Pierre Lebigre <pierre.lebigre@outlook.fr>
2023-03-22 15:57:48 +0100

    Add different tf file

--- main.tf
+++ main.tf
@@ -5,15 +5,27 @@ terraform {
       version = "3.48.0"
     }
   }
-}
 
-provider "azurerm" {
-  features {
-    
+  backend "azurerm" {
   }
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "rg-plebigre"
-  location = "West Europe"
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_service_plan" "app-plan" {
+  name                = "plan-${var.project_name}${var.environment_suffix}"
+  resource_group_name = data.azurerm_resource_group.rg-maalsi.name
+  location            = data.azurerm_resource_group.rg-maalsi.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "web-${var.project_name}${var.environment_suffix}"
+  resource_group_name = data.azurerm_resource_group.rg-maalsi.name
+  location            = data.azurerm_resource_group.rg-maalsi.location
+  service_plan_id     = azurerm_service_plan.app-plan.id
+  site_config {}
 }
